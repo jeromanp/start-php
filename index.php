78617b37ea734edfd7b5aa02f788f4a3a68c3dd3
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-29 23:39:19 -0600

    const and if

--- index.php
+++ index.php
@@ -3,6 +3,8 @@ $name = "Eduardo";
 $isDev = true;
 $age = 33;
 
+
+
 // var_dump($name); //Muestra el tipo de dato más el valor
 // var_dump($isDev);
 // var_dump($age);
@@ -13,9 +15,38 @@ $age = 33;
 
 $output = "Hola " . $name . " tiene <br/>" . $age . " años";
 
-define("LOGO_URL", "https://www.php.net//images/logos/new-php-logo.svg")
+//Constante
+define("LOGO_URL", "https://www.php.net//images/logos/new-php-logo.svg");
+const NOMBRE = "Jose Eduardo";
+
+//If
+$isOld = $age > 40;
 ?>
-<img src="<?=LOGO_URL?>" alt="PHP Logo" width="200">
+
+<?php
+if ($isOld) {
+    echo "<h2>Eres Viejo, lo siento</h2>";
+} elseif ($isDev) {
+    echo "<h2>Eres un Desarrollador, estás aquí para aprender</h2>";
+} else {
+    echo "<h2>Eres Joven, felicidades</h2>";
+}
+?>
+
+<!-- //If segunda opcion -->
+<?php if ($isOld) : ?>
+    <h2>Eres Viejo, lo siento</h2>
+
+<?php elseif ($isDev) : ?>
+    <h2>Eres un Desarrollador, estás aquí para aprender</h2>
+<?php else : ?>
+    <h2>Eres Joven, felicidades</h2>
+
+<?php endif; ?>
+
+
+<!-- //Imagenes -->
+<img src="<?= LOGO_URL ?>" alt="PHP Logo" width="200">
 
 <h1>
     <?=
@@ -28,6 +59,8 @@ define("LOGO_URL", "https://www.php.net//images/logos/new-php-logo.svg")
     "Hola " . $name . " tiene <br/>" . $age . " años";
     ?>
 </h1>
+
+
 
 <style>
     :root {
