5762189fd149d3536f1fbd1a87c7c653384843a9
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-30 23:26:58 -0600

    IF MATCH TERNARY

--- index.php
+++ index.php
@@ -3,8 +3,6 @@ $name = "Eduardo";
 $isDev = true;
 $age = 33;
 
-
-
 // var_dump($name); //Muestra el tipo de dato más el valor
 // var_dump($isDev);
 // var_dump($age);
@@ -15,44 +13,63 @@ $age = 33;
 
 $output = "Hola " . $name . " tiene <br/>" . $age . " años";
 
-//Constante
+//Definir Constante
 define("LOGO_URL", "https://www.php.net//images/logos/new-php-logo.svg");
 const NOMBRE = "Jose Eduardo";
 
-//If
+//Componente Ternario
 $isOld = $age > 40;
-?>
+$outputAge = $isOld
+    ? "Eres viejo"
+    : "Eres joven";
 
+?>
+<h2><?="Ternario: $outputAge" ?></h2>
+
+
+<!-- If primero opción -->
 <?php
 if ($isOld) {
-    echo "<h2>Eres Viejo, lo siento</h2>";
+    echo "<h2>IF1: Eres Viejo, lo siento</h2>";
 } elseif ($isDev) {
-    echo "<h2>Eres un Desarrollador, estás aquí para aprender</h2>";
+    echo "<h2>IF1: Eres un Desarrollador, estás aquí para aprender</h2>";
 } else {
-    echo "<h2>Eres Joven, felicidades</h2>";
+    echo "<h2>IF1: Eres Joven, felicidades</h2>";
 }
 ?>
 
-<!-- //If segunda opcion -->
+<!-- //If segunda opción -->
 <?php if ($isOld) : ?>
-    <h2>Eres Viejo, lo siento</h2>
+    <h2>IF2: Eres Viejo, lo siento</h2>
 
 <?php elseif ($isDev) : ?>
-    <h2>Eres un Desarrollador, estás aquí para aprender</h2>
+    <h2>IF2: Eres un Desarrollador, estás aquí para aprender</h2>
 <?php else : ?>
-    <h2>Eres Joven, felicidades</h2>
+    <h2>IF2: Eres Joven, felicidades</h2>
 
 <?php endif; ?>
+
+<!-- Match -->
+<?
+$outputAgeMatch = match ($age) {
+    0, 1, 2 => "Eres un bebé , $name",
+    3, 4, 5, 6, 7, 8, 9 => "Eres un niño, $name",
+    10, 11, 12 => "Eres un adolescente, $name",
+    13, 14, 15, 16, 17, 18, 19 => "Eres un joven, $name",
+    default=> "Eres un adulto $name",
+}
+?>
+<h2><?="Match: $outputAgeMatch" ?></h2>
 
 
 <!-- //Imagenes -->
 <img src="<?= LOGO_URL ?>" alt="PHP Logo" width="200">
 
-<h1>
+<!-- <h1>
     <?=
     $output
     ?>
-</h1>
+</h1> -->
 
 <h1>
     <?=
