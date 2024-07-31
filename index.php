45fc19e8591f68d2caf67bf7e67cac08c54a56a4
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-07-31 00:04:25 -0600

    match

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
+<!-- Levantar proyecto: php -S localhost:8000 -->
+
 <?php
 $name = "Eduardo";
 $isDev = true;
-$age = 33;
+$age = 18;
 
 // var_dump($name); //Muestra el tipo de dato más el valor
 // var_dump($isDev);
@@ -24,7 +26,7 @@ $outputAge = $isOld
     : "Eres joven";
 
 ?>
-<h2><?="Ternario: $outputAge" ?></h2>
+<h2><?= "Ternario: $outputAge" ?></h2>
 
 
 <!-- If primero opción -->
@@ -49,17 +51,30 @@ if ($isOld) {
 
 <?php endif; ?>
 
-<!-- Match -->
+<!-- Match (opción con más código) -->
 <?
 $outputAgeMatch = match ($age) {
     0, 1, 2 => "Eres un bebé , $name",
     3, 4, 5, 6, 7, 8, 9 => "Eres un niño, $name",
     10, 11, 12 => "Eres un adolescente, $name",
     13, 14, 15, 16, 17, 18, 19 => "Eres un joven, $name",
-    default=> "Eres un adulto $name",
+    default => "Eres un adulto $name",
 }
 ?>
-<h2><?="Match: $outputAgeMatch" ?></h2>
+<h2><?= "Match 1: $outputAgeMatch" ?></h2>
+
+<!-- Match (opción ideal) -->
+<?
+$outputAgeMatch = match (true) {
+    $age < 2 => "Eres un bebé , $name",
+    $age < 10 => "Eres un niño, $name",
+    $age < 18 => "Eres un adolescente, $name",
+    $age === 18 => "Eres mayor de edad $name",
+    $age < 20 => "Eres un joven, $name",
+    default => "Eres un adulto $name",
+}
+?>
+<h2><?= "Match 2: $outputAgeMatch" ?></h2>
 
 
 <!-- //Imagenes -->
